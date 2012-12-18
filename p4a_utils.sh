c6670c1dcfd03992caceb6f39f8e83e0b18df737
tshirtman <gabriel.pettier@gmail.com>
2012-12-18 10:08:35 +0100

    add p4a_init in p4a_utils

--- p4a_utils.sh
+++ p4a_utils.sh
@@ -1,7 +1,12 @@
 # python for android build helpers
-#
+PYTHON_FOR_ANDROID=~/python-for-android
+
 function lg(){
-    ssh $1 logcat | grep -i python| logcat
+    if [ "x$1" == "xadb" ]; then
+        adb logcat | grep -i python
+    else
+        ssh $1 logcat | grep -i python
+    fi
 }
 
 function deploy(){
@@ -24,34 +29,64 @@ function deploy(){
 }
 
 function build(){
-    rm ${KIVYANDROID}bin/*
+    rm ${KIVYANDROID}bin/*.apk
     dir=$PWD
     pushd ${KIVYANDROID}
+
+    for p in $P4A_permissions
+	    permissions="--permission $p $permissions"
+
     ./build.py\
 	    --dir $dir\
 	    --package $P4A_package\
 	    --name "$P4A_name"\
 	    --version $P4A_version\
-	    $P4A_permissions\
+	    $permissions\
 	    debug
     popd
+    unset permissions
+
     if [ $1 ]; then
-        deploy $1
-        lg $1
+	    deploy $1
+	    lg $1
     fi
 }
 
 function buildall(){
-    build
-    for i in $@; do
-    	deploy $i
-    done
+build
+for i in $@; do
+	deploy $i
+done
 }
 
 function get_pid(){
-   ssh $1 "ps | grep $P4A_package"|tr -s ' ' ' '|cut -d' ' -f2
+ssh $1 "ps | grep $P4A_package"|tr -s ' ' ' '|cut -d' ' -f2
 }
 
 function rkill(){
-   ssh $1 "kill $(get_pid $1)"
+ssh $1 "kill $(get_pid $1)"
+}
+
+function p4a_init(){
+select dist in $(ls $PYTHON_FOR_ANDROID/dist/)
+	break
+	echo "package name?"
+	read package
+	echo "app name?"
+	read name
+	echo "app version?"
+	read version
+
+	<<EOF > .env
+	export KIVYANDROID=$PYTHON_FOR_ANDROID/dist/$dist/
+	export P4A_package=$package
+	export P4A_name=$name
+	export P4A_version=$version
+	export P4A_permissions=
+EOF
+   unset dist
+   unset package
+   unset name
+   unset version
+   cd .
 }
